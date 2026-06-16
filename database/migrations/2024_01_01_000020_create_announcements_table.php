bb0d82720c5dcfd9e305390a72c607b83b6c11c7
Agung Mustaqim <agungmustaqim15@gmail.com>
2026-06-16 14:20:23 +0700

    asdasd

---
+++ database/migrations/2024_01_01_000020_create_announcements_table.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // TODO: define schema for this table.
+    }
+
+    public function down(): void
+    {
+        // TODO: reverse schema changes.
+    }
+};
